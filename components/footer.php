bbcdf20f56ba05842a3731357b5d6d0ca7f6933f
Sumaiya Shah <108864503+sumaiyashah27@users.noreply.github.com>
2025-03-16 00:11:25 +0530

    Update footer.php

--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
 
-   &copy; copyright @ <?= date('Y'); ?> by <span>mr. web designer</span> | all rights reserved!
+   &copy; copyright @ <?= date('Y'); ?> by <span>Sumaiya_Shah</span> | all rights reserved!
 
 </footer>
